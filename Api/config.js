65ecae5c09a09dec22923bacbbfd0238be4d007b
Enmanuel <enmareco.co@gmail.com>
2024-12-02 19:01:40 -0300

    update uuid

--- Api/config.js
+++ Api/config.js
@@ -4,33 +4,30 @@ dotenv.config();
 
 const FRONTEND_URL =
   process.env.NODE_ENV === "production"
-    ? process.env.FRONTEND_URL_GIO
+    ? process.env.FRONTEND_URL
     : "http://localhost:5173";
 
 const DB_HOST =
-  process.env.NODE_ENV === "production" ? process.env.DB_HOST_GIO : "localhost";
+  process.env.NODE_ENV === "production" ? process.env.DB_HOST : "localhost";
 const DB_USER =
-  process.env.NODE_ENV === "production" ? process.env.DB_USER_GIO : "postgres";
+  process.env.NODE_ENV === "production" ? process.env.DB_USER : "postgres";
 const DB_NAME =
-  process.env.NODE_ENV === "production" ? process.env.DB_NAME_GIO : "bygio";
+  process.env.NODE_ENV === "production" ? process.env.DB_NAME : "bygio";
 const DB_PASSWORD =
-  process.env.NODE_ENV === "production" ? process.env.DB_PASSWORD_GIO : "Enma2707";
+  process.env.NODE_ENV === "production" ? process.env.DB_PASSWORD : "Enma2707";
 const DB_PORT =
-  process.env.NODE_ENV === "production" ? process.env.DB_PORT_GIO : 5432;
-const DB_URL =
-  process.env.NODE_ENV === "production" && process.env.DB_URL;
+  process.env.NODE_ENV === "production" ? process.env.DB_PORT : 5432;
+const DB_URL = process.env.NODE_ENV === "production" && process.env.DB_URL;
 
-const DB_DIALECT =
-  process.env.NODE_ENV === "production" ? "mysql" : "postgres";
+const DB_DIALECT = process.env.NODE_ENV === "production" ? "mysql" : "postgres";
 
-  module.exports = {
-    FRONTEND_URL,
-    DB_HOST,
-    DB_USER,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_PORT,
-    DB_URL,
-    DB_DIALECT,
-  };
-  
+module.exports = {
+  FRONTEND_URL,
+  DB_HOST,
+  DB_USER,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_URL,
+  DB_DIALECT,
+};
