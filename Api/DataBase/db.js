1dc5a9ecea04068037e377f946677198ce143be2
Enma Reyes <enmareco.co@gmail.com>
2026-09-07 13:55:06 -0300

    cambio en db autenticator

--- Api/DataBase/db.js
+++ Api/DataBase/db.js
@@ -1,28 +1,16 @@
-// DataBase/db.js
 const { Sequelize } = require("sequelize");
 const { DATABASE_URL, DB_DIALECT } = require("../config.js");
 
-const sequelizeOptions = {
+const sequelize = new Sequelize(DATABASE_URL, {
   dialect: DB_DIALECT || "postgres",
   dialectModule: require("pg"),
   logging: false,
-  dialectOptions: {},
-};
-
-if (process.env.NODE_ENV === "production") {
-  sequelizeOptions.dialectOptions = {
+  dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
-  };
-}
-
-const sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
-
-sequelize
-  .authenticate()
-  .then(() => console.log("Conexión exitosa con la base de datos"))
-  .catch((err) => console.error("Error conectando con la base de datos:", err));
+  },
+});
 
 module.exports = { sequelize };
